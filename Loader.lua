493d9921eb4598f8759e0f0a552017676669c5d0
zikkasep <zik16248@gmail.com>
2025-07-06 22:06:33 +0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,6 @@
+local Settings = {
+    JoinTeam = "Pirates", -- Ganti ke "Marines" kalau mau
+    Translator = true
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zikkasep/Zikpro/main/MainUI.lua"))(Settings)
